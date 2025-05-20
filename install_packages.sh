fd3cf66ccba3b7da93b21ae643362debecec9d07
Zachary Everton <33760323+evergreen700@users.noreply.github.com>
2025-05-20 13:09:21 -0600

    Create install_packages.sh

---
+++ install_packages.sh
@@ -0,0 +1,11 @@
+sudo apt-get -y update
+sudo apt-get -y install vim
+
+echo "***"
+echo "INSTALLING MINICONDA"
+echo "***"
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
